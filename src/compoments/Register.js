23d8b6470e39388f70b457d49c2e8ce6fc7f6263
longntgcd17099 <longntgcd17099@fpt.edu.vn>
2021-03-22 18:54:50 +0700

    themvalidate

--- src/compoments/Register.js
+++ src/compoments/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import axios from 'axios'
+import { validateSignup } from "./../validationErr/validateSignup"
 
 class Register extends Component {
     constructor() {
@@ -10,7 +11,8 @@ class Register extends Component {
             username: '',
             email: '',
             password: '',
-            faculty: 'It'
+            faculty: 'It',
+            errors: {}
         }
     }
 
@@ -29,18 +31,38 @@ class Register extends Component {
     facultyChange = event => {
         this.setState({ faculty: event.target.value })
     }
+    clearState = () => {
+        this.setState({
+            fullName: '',
+            username: '',
+            email: '',
+            password: '',
+            faculty: 'It'
+        })
+    }
+    validate = (fullName, username, faculty, email, password) => {
+        const errors = validateSignup(fullName, username, faculty, email, password);
+        this.setState({ errors: errors });
+        return Object.values(errors).every((err) => err === "");
+    };
     onSubmit = event => {
-        //event.preventDefault()
-
-        const registered = {
-            fullName: this.state.fullName,
-            username: this.state.username,
-            email: this.state.email,
-            password: this.state.password,
-            faculty: this.state.faculty
+        event.preventDefault();
+        const { fullName, username, faculty, email, password } = this.state;
+        if (this.validate(fullName, username, faculty, email, password)) {
+            const registered = {
+                fullName: this.state.fullName,
+                username: this.state.username,
+                email: this.state.email,
+                password: this.state.password,
+                faculty: this.state.faculty
+            }
+            console.log(registered)
+            axios.post('http://localhost:4000/app/signup', registered).then(response => console.log(response.data)).
+            then(res => {
+            alert('Please wait for the confirmation of the account from Admin!')
+            this.clearState()
+        } )
         }
-        console.log(registered)
-        axios.post('http://localhost:4000/app/signup', registered).then(response => console.log(response.data))
     }
 
     render() {
@@ -54,23 +76,35 @@ class Register extends Component {
                                 onChange={this.changeFullName}
                                 value={this.state.fullName}
                                 className="form-control form-group" />
+                            {this.state.errors.fullName && (
+                                <div className="text-danger">{this.state.errors.fullName}</div>
+                            )}
 
                             <input type="text"
                                 placeholder=' UserName'
                                 onChange={this.changeUsername}
                                 value={this.state.username}
                                 className="form-control form-group" />
+                            {this.state.errors.fullName && (
+                                <div className="text-danger">{this.state.errors.fullName}</div>
+                            )}
 
                             <input type="text"
                                 placeholder='E-mail'
                                 onChange={this.changeEmail}
                                 value={this.state.email}
                                 className="form-control form-group" />
+                            {this.state.errors.email && (
+                                <div className="text-danger">{this.state.errors.email}</div>
+                            )}
                             <input type="password"
                                 placeholder='Password'
                                 onChange={this.changePassword}
                                 value={this.state.password}
                                 className="form-control form-group" />
+                            {this.state.errors.password && (
+                                <div className="text-danger">{this.state.errors.password}</div>
+                            )}
                             <select className="form-select form-select-lg mb-3" aria-label="Default select example" onChange={this.facultyChange}>
                                 <option selected>Select Faculty</option>
                                 <option value="It">It</option>
